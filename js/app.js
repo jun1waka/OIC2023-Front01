bb45fa183af1fb2574ebf3762b7b13e8cd2e95d6
JUNICHI WAKABAYASHI <jun1@rimgate.net>
2023-05-22 10:37:34 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,9 +8,10 @@ class Card {
 const cards=[];
 const card_type = ['s','d','h','c']
 let count = 0
-for(let i=0;i<card_type.length;i++{
-  for(let j=1;j<=13;i++){
-    let card[count]= new Card(card_type[i],j);
+for(let i=0;i<card_type.length;i++){
+  for(let j=1;j<=13;j++){
+    let cards[count]= new Card(card_type[i],j);
     console.log(count +'type:' + card_type[i] + ' num:' + j);
+     count++;
   }
 }
